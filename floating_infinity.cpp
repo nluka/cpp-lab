2a4dc92ade4e52b000a2d63c02c35b0e7253a38e
Nicholas Lukasevich <n.luka784@gmail.com>
2023-04-08 14:15:53 -0400

    NaN

--- floating_infinity.cpp
+++ floating_infinity.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <limits>
+#include <cmath>
 
 int main() {
   double pos_inf = std::numeric_limits<double>::infinity();
   double neg_inf = -std::numeric_limits<double>::infinity();
+  double pos_nan = std::nan("");
+  double neg_nan = -std::nan("");
 
   std::cout << "pos_inf = " << pos_inf << '\n'
-            << "neg_inf = " << neg_inf << '\n';
+            << "neg_inf = " << neg_inf << '\n'
+            << "pos_nan = " << pos_nan << '\n'
+            << "neg_nan = " << neg_nan << '\n'
+  ;
 }
